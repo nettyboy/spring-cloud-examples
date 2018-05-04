6cbff8f494137b6b13e08fae27e7d1c4aed7afc9
netty <nq921665793@126.com>
2018-05-04 16:37:09 +0800

    修改配置文件

---
+++ microservice-cloud-config/microservice-config-client/src/main/java/com/zhht/controller/ConfigTestController.java
@@ -0,0 +1,19 @@
+package com.zhht.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/testconfig")
+public class ConfigTestController {
+
+	@Value("profile")
+	private String applicationProfile;
+	
+	@GetMapping("/getprofile")
+	public String getProfile() {
+		return applicationProfile;
+	}
+}
