b26934e0583e30e3f4f3422fa928c07a77cee6fd
netty <nq921665793@126.com>
2018-05-06 13:15:39 +0800

    client pattern

---
+++ microservice-cloud-config/microservice-config-server-pattern/src/main/java/com/zhht/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.zhht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient //激活对eureka注册服务的支持
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
