f1d154a27208bcf54906bda042b5e3e196e6d412
ningquan <ningquan@aipark.com>
2018-09-26 16:20:45 +0800

    修改microservice-bus-config

--- microservice-cloud-config/microservice-config-client-rabbit/src/main/java/com/zhht/controller/ConfigTestController.java
+++ microservice-cloud-config/microservice-config-client-rabbit/src/main/java/com/zhht/controller/ConfigTestController.java
@@ -1,10 +1,20 @@
 package com.zhht.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/mytest")
 public class ConfigTestController {
+    
+    @Value("${profile}")
+    private String applicationProfile;
+    
+    @GetMapping("/getprofile")
+    public String getProfile() {
+        return applicationProfile;
+    }
 
 }
